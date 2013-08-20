613c0bc73d09252139df992ee7d87f94b75e4cd5
Tom Spoon <spoon@blah.net>
2013-08-20 16:29:23 -0400

    topsort is working now.  Might move it to a core module.

--- src/spork/cljgraph/algorithms/topsort.clj
+++ src/spork/cljgraph/algorithms/topsort.clj
@@ -10,18 +10,18 @@
   (reduce #(top/disj-node %1 %2) g (get-roots g)))
 
 (defn topsort
-  "Topologically sort the graph.  We use a priority queue to maintain a fringe 
-   of nodes to visit, where the priorities are the number of sources to a node.
-   We pull arcs off of the "
+  "Topologically sort the graph, returning a sequence of sets of topological 
+   roots.  Multiple valid topological orderings may be returned from the
+   result."
   [g]
-	(loop [the-graph  g
+	(loop [graph g
          roots (set (get-roots g)) ;;current roots in g 
          acc []]
      (if (seq roots)
-       (let [xs  (mapcat (partial top/sinks the-graph)  roots) ;only possible new roots.
-             next-graph  (top/drop-nodes the-graph roots)]
+       (let [xs  (mapcat (partial top/sinks graph)  roots) ;only possible new roots.
+             next-graph  (top/drop-nodes graph roots)]
          (recur next-graph (set (get-roots next-graph xs))  (conj acc roots)))
-       (if (= (reduce + (map count roots)) 
+       (if (= (reduce + (map count acc)) 
               (count (top/nodes g)))
            acc
            nil))))
@@ -30,4 +30,51 @@
   (persistent! 
     (reduce (fn [acc xs] (reduce conj! acc xs)) (transient []) (topsort g)))) 
 
-    
+;;testing
+(comment
+  
+;a directed graph...
+ ;    a -> b -> d
+ ;          \. ^
+ ;           c/
+ ;    e -> f/^
+ 
+ (def the-graph 
+   (-> top/empty-topograph
+       (top/add-arcs [[:a :b] [:b :c] [:c :d] [:e :f] [:f :c] [:b :d]])))
+
+ (assert (= (topsort the-graph)  [#{:a :e} #{:b :f} #{:c} #{:d}]))
+ (assert (= (topological-order the-graph) [:a :e :b :f :c :d]))
+
+ ;a directed graph with 2 components
+ ;    a -> b -> d
+ ;          \. ^
+ ;           c/
+ ;    e -> f
+ 
+ (def the-graph2 
+   (-> top/empty-topograph
+       (top/add-arcs [[:a :b] [:b :c] [:c :d] [:e :f] [:b :d]])))
+ (assert (= (topsort the-graph2) [#{:a :e} #{:b :f} #{:c} #{:d}])) 
+ (assert (= (topological-order the-graph2)
+            [:a :e :b :f :c :d]))
+ 
+ ;a directed graph with 2 components
+ ;    a -> b -> d
+ ;          \. ^
+ ;           c/
+ ;    e -> f
+ ;    g 
+ ;    h
+ 
+ (def the-graph3
+   (-> top/empty-topograph
+     (top/add-arcs [[:a :b] [:b :c] [:c :d] [:e :f] [:b :d]])
+     (top/conj-node :g)
+     (top/conj-node :h)))
+ (assert (= (topsort the-graph3) [#{:a :g :e :h} #{:b :f} #{:c} #{:d}]))
+ (assert (= (topological-order the-graph3) [:a :g :e :h :b :f :c :d]))    
+)
+
+
+
