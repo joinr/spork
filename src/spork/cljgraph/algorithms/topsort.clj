26eaf8ba0c014cb83a7f38d9b324685c3bce98ae
Tom Spoon <spoon@blah.net>
2013-08-20 15:21:02 -0400

    Got topsort squared away.

--- src/spork/cljgraph/algorithms/topsort.clj
+++ src/spork/cljgraph/algorithms/topsort.clj
@@ -3,49 +3,31 @@
   (:require [spork.util [topographic :as top]]))
 
 (defn- get-roots 
-  [g]
-  (->> (top/get-node-labels g)    
-       (filter #(= 0 (count (top/sources g %))))))
+  ([g xs] (filter #(top/source-node? g %) xs))
+  ([g] (get-roots g (top/get-node-labels g))))
         
 (defn- drop-roots [g]
-  (reduce #(top/drop-node %1 %2) g (get-roots g)))
-
-;;##Check your logic, I don't think these do topsort like intended.
+  (reduce #(top/disj-node %1 %2) g (get-roots g)))
 
 (defn topsort
-  "Topologically sort the graph.  This is more efficient, in theory, 
-   because it only calls get-roots to initialize the sort.  From there, 
-   we determine which nodes will be nextroots, and queue them up, until 
-   no nodes are left.  We avoid the book-keeping costs from dropping nodes
-   in the graph."
+  "Topologically sort the graph.  We use a priority queue to maintain a fringe 
+   of nodes to visit, where the priorities are the number of sources to a node.
+   We pull arcs off of the "
   [g]
-	(loop [roots (set (get-roots g)) 
-	       visited roots
-         acc [(seq roots)]]
+	(loop [the-graph  g
+         roots (set (get-roots g)) ;;current roots in g 
+         acc []]
      (if (seq roots)
-       (let [nextroots (->> roots
-                            (mapcat (partial top/sinks g))
-                            (filter #(not (contains? visited %))))                
-             nextacc   (if (seq nextroots) (conj acc nextroots) acc)]            
-                                   
-         (recur (set nextroots) (clojure.set/union visited roots) nextacc))
-       (if (= (count visited) 
+       (let [xs  (mapcat (partial top/sinks the-graph)  roots) ;only possible new roots.
+             next-graph  (top/drop-nodes the-graph roots)]
+         (recur next-graph (set (get-roots next-graph xs))  (conj acc roots)))
+       (if (= (reduce + (map count roots)) 
               (count (top/nodes g)))
            acc
            nil))))
 
+(defn topological-order [g]
+  (persistent! 
+    (reduce (fn [acc xs] (reduce conj! acc xs)) (transient []) (topsort g)))) 
 
-(defn topsort-seq
-  "Return a lazy sequence of topologically-sorted nodes, if there is one."
-  [g]
-	(if-let [roots (get-roots g)]
-     (let [visited (atom (set (get-roots g)))
-           valid? (fn [n] 
-                     (if-not (contains? @visited n)
-                       (do (swap! visited conj n)
-                            true)
-                        false))
-           next-roots (fn [nodes] (->> nodes
-                                    (mapcat (partial top/get-sinks g))
-                                    (filter valid?)))]
-       (take-while #(seq %) (iterate next-roots roots))))) 
+    
