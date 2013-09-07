f6b7b20bfffcc1d9ba4d3b7cd333eb01bccf8f78
Tom <tom@demo.com>
2013-09-07 00:35:20 -0400

    Forgot to add custom strokes and geometric primitives into vc.

---
+++ src/spork/graphics2d/primitive.clj
@@ -0,0 +1,39 @@
+;;An implementation of segmented paths and primitive verticee.  Used as a 
+;;primitive facility for defining shapes and constructive geometry.
+;;Work in progress, should free us from J2d entirely...
+(ns spork.graphics2d.primitive
+  (:require [spork.util [vectors :as v]])
+  (:import  [spork.util.vectors vec2 vec3]))
+
+(defn ->point [^double x ^double y] (v/->vec2 x y))
+(defn ^double point-x [^vec2 v] (:x0 v))
+(defn ^double point-y [^vec2 v] (:x1 v))
+
+(def origin (->point 0.0 0.0))
+
+;;adapted from j2d, not sure i'll need it though.
+(defn segment-type [^long id] 
+  (case id
+    0 :close
+    1 :cubic
+    2 :line
+    3 :quad
+    4 :move 
+    5 :even
+    6 :nonzero))
+
+(defrecord seg [^long id ^vec2 p1 ^vec2 p2  ^vec2 p3])
+(defn line-to  [^double x ^double y] 
+  (->segment 2 (->point x y) origin origin))
+(defn curve-to [^double x ^double y ^double x2 ^double y2 ^double x3 ^double y3] 
+  (->segment 1 (->point x y) (->point x2 y2) (->point x3 y3)))
+(defn quad-to  [^double x ^double y ^double x2 ^double y2] 
+  (->segment 3 (->point x y) (->point x2 y2) origin))
+
+;;a path is defined as a sequence of segments. 
+;;so...anything can be a path if it can provide a sequence of 
+;;segments...
+
+(defprotocol ISegmented
+  (segment-path [s]   "Return a sequence of segments that define the shape."))
+
---
+++ src/spork/graphics2d/strokes.clj
@@ -0,0 +1,72 @@
+;;A wrapper for some strokes, for stylized drawing.
+(ns spork.graphics2d.strokes
+  (:import  [java.awt Shape Stroke BasicStroke ]
+            [java.awt.geom  GeneralPath PathIterator FlatteningPathIterator]))
+
+(defn ^FlatteningPathIterator shape->path 
+  ([^Shape s ^Double flatness]
+    (FlatteningPathIterator. (.getPathIterator s nil) flatness))
+  ([^Shape s] (shape->path s 1.0)))  
+
+(defn ^Shape shape->stroked [^Shape s] (.createStrokedShape (BasicStroke. 10) s))   
+(defn path-done? [^PathIterator p] (.isDone p))
+
+(defn ^doubles ->segment [& xs]
+  (double-array (take 6 (if (seq xs) xs (repeat 0.0)))))
+
+(defn ^float randomize 
+  ([^double amplitude ^double x]  (float (+ x (- (* (rand) amplitude) 1.0))))
+  ([^double x] (float (randomize 2.0 x))))
+      
+ ;;Note...this is basically just a little state machine.
+ ;;We can abstract this much better...
+;;Pattered off of Jerry Huxtable's excellent demo code in java.
+(defn ^GeneralPath jittered-path [detail jitter ^Shape s]
+  (let [^GeneralPath the-path (GeneralPath.)
+        ^PathIterator it (shape->path (shape->stroked s))        
+        points  (float-array 6)]
+    (loop [movex   (float 0.0)
+           movey   (float 0.0)
+           lastx   (float 0.0) 
+           lasty   (float 0.0)
+           thisx   (float 0.0)
+           thisy   (float 0.0)
+           first   false
+           next    (float 0.0)]
+      (if (path-done? it) the-path
+        (case (.currentSegment it points)
+          PathIterator/SEG_MOVETO
+          (let [movex (float (randomize (aget points 0)))
+                lastx (float movex)
+                movey (float (randomize (aget points 1)))
+                lasty (float movey)]
+            (do (.moveto the-path (float movex) ^float (float movey))
+              (recur movex movey lastx lasty thisx thisy true 0.0)))
+          PathIterator/SEG_CLOSE
+          (do (aset points 0 (float movex))
+            (aset points 1 (float movey))
+            (recur movex movey lastx lasty thisx thisy first next))
+          PathIterator/SEG_LINETO
+          (let [thisx (float (randomize (aget points 0)))
+                thisy (float (randomize (aget points 1)))
+                dx    (- thisx lastx)
+                dy    (- thisy lasty)
+                distance (Math/sqrt (+ (* dx dx) (* dy dy)))
+                next-nxt
+                   (float (if (< distance next)  (- next distance)                 
+                     (let [r (/ 1.0 distance)
+                           angle (Math/atan2  dy dx)]
+                       (loop [distance (float distance)
+                              next (float next)]
+                         (if (< distance next) (- next distance)
+                           (let [x (float (randomize (+ lastx (* next dx r))))
+                                 y (float (randomize (+ lasty (* next dy r))))]
+                             (do (.lineto the-path  x y)
+                                 (recur distance (float (+ next detail))))))))))]
+            (recur movex movey thisx thisy thisx thisy false next-nxt))
+          (do (.next it)
+            (recur movex movey lastx lasty thisx thisy first next)))))))
+ 
+(defn ->jitter-stroke [detail jitter]
+  (reify java.awt.Stroke
+    (createStrokedShape [this shape] (jittered-path detail jitter shape))))  
