2c4dfb77aaec81eaf3a20b8e5105cfa8755c23c0
Tom Spoon <spoon@blah.net>
2013-09-30 11:22:48 -0400

    Adding dumb multiobjective portfolio stuff.

--- src/spork/opt/dumbga.clj
+++ src/spork/opt/dumbga.clj
@@ -26,9 +26,45 @@
 ;;individuals, and joins the results of breeding into the new
 ;;population.
 
-;;we'll assume that individuals are homogenous (i.e. the same
+;;We'll assume that individuals are homogenous (i.e. the same
 ;;dimension) real valued vectors.  Thus, a population is merely a
 ;;collection of these vectors.  This makes our fitness function easy
 ;;to define as well: it's simply a mapping of a real valued vector to
 ;;a floating point value.
 
+(defrecord ga-parameters [size fitness breed select join])
+
+;;Both selection and breeding are pretty diverse topics.  We'll allow 
+;;the caller to define specific implementations for both.  Breeding,
+;;in general, maps a population to another population.  Selection is 
+;;usually a function that maps a population to a sub-population.
+;;We use selection, however it's performed, to provide an initial
+;;population argument for breeding.  Breeding returns a set of
+;;new candidates derived from the initial population (somehow).  The
+;;result of breeding is then passed to a join operator, which
+;;integrates the new population with the old. 
+
+(defn get-best [m]   (get m :best)
+(defn assess-fitness [f xs]
+  (persistent! 
+   (reduce (fn [acc [idx x]] 
+             (let [^double v (fitness x)]                                                                                         (-> (if (> v (get-best m))                                                                                            (assoc! acc :best idx)                                                                                             acc)                                                                                                             (assoc! acc idx (fitness x)))))
+           (transient {}) (map-indexed xs))))
+
+;;A simple implementation of a genetic algorithm: 
+(defn ga-solve 
+  "Given parameters for a genetic algorithm, and an initial population, 
+   continually breeds, selects, joins, the population until a stopping 
+   criterion is met.  Fitness is inferred to mean more fit = higher value. 
+   This is a stylistic choice, which differs from other methods.  As such, 
+   individuals compared by fitness will prefer a higher fitness value.  In 
+   the context of ga, I think it's more intuitive.  Callers need to enforce
+   this mechanism."
+  [{:keys [size fitness breed select join] :as params} init-pop terminate?]
+  (let [asess (partial asses-fitness fitness)]
+    (loop [pop    init-pop
+           scores (asses-fitness init-pop)
+           best   
+           ]
+
+  
