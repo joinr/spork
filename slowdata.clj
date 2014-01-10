827f18acbf92d754e0a942c69800abd59156fbf9
Tom Spoon <spoon@blah.net>
2014-01-10 14:54:01 -0500

    Added optimizations to digraph.

---
+++ slowdata.clj
@@ -0,0 +1,72 @@
+(ns testgraph
+  (:require [spork.cljgraph [flow :as flow] 
+                            [core :as graph]]))
+
+;;This is a chunk of data that exposes some flaws in the graph algos.
+;;Good test data for netflow stuff.
+;;Seems like the priority-first-search is slow...
+(def g 
+#spork.data.digraph.digraph{:nodes
+                            {:filled 7
+                             "5530LJ00" 6
+                             RCAD-BIG 5
+                             RCAD 4
+                             RC 3
+                             AC 2
+                             Supply 1
+                             Total 0}
+                            :sources
+                            {:filled {"55530LJ00" 0}
+                             "55530LJ00" {AC 0, RCAD 1}
+                             RCAD-BIG {RC 0}
+                             RCAD {RC 0}
+                             RC {Supply 0}
+                             AC {Supply 0}
+                             Supply {Total 0}
+                             Total {}}
+                            :sinks 
+                            {:filled {}
+                             "55530LJ00" {:filled 0}
+                             RCAD-BIG {}
+                             RCAD {"55530LJ00" 1}
+                             RC {RCAD 0 RCAD-BIG 0}
+                             AC {"55530LJ00" 0}
+                             Supply {AC 0 RC 0}
+                             Total {Supply 0}}
+                            :flow-info 
+                            {["55530LJ00" :filled]
+                             {:from "55530LJ00" :to :filled :capacity 31.0 :flow 0}
+                             [RCAD "55530LJ00"]
+                             {:from RCAD
+                              :to "55530LJ00"
+                              :capacity 9223372036854775807
+                              :flow 0}
+                             [AC "5530LJ00"]
+                             {:from AC :to "55530LJ00" :capacity 9223372036854775807 :flow 0}
+                             [RC RCAD-BIG] {:from RC :to RCAD-BIG :capacity 25000 :flow 0}
+                             [RC RCAD]     {:from RC :to RCAD :capacity 25000 :flow 0}
+                             [Supply RC]   {:from Supply :to RC :capacity 530000 :flow 0}
+                             [Supply AC]   {:from Supply :to AC :capacity 450000 :flow 0}
+                             [Total Supply] {:from Total :to Supply :capacity 980000 :flow 0}}})
+   
+
+;;We can get a significant performance boost (x 5) by eliminating the call
+;;to vec in the implementation of -get-sinks and -get-sources for
+;;spork.data.digraph.digraph - just return 
+
+;;Also, I use ordered maps for the digraph.
+;;These are about 2x as slow for seq instancing as normal maps...
+;;Another optimization is to eliminate ordered maps and just use {} 
+;;That lets us use array maps where possible (dictated by the runtime).
+
+;;We only really need ordered traversal for tree operations...
+;;Thus, I don't think the ordered graph should be the default.
+;;There's overhead incurred every time there...
+
+;;Another likely performance killer is the priorityq implementation
+;;for the search fringes. 
+
+;;Fringes, in general, will be transient in nature: They only exist to
+;;facilitate the search.  Since we want searches to be as fast as
+;;possible, we probably want to use mutable structures for the
+;;fringes. That should be an assload faster. 
--- src/spork/data/digraph.clj
+++ src/spork/data/digraph.clj
@@ -14,36 +14,118 @@
 (defrecord digraph [nodes sources sinks]
   ITopograph
   (-get-nodes [tg] nodes)
-  (-set-nodes [tg m] (digraph. m sources sinks))
+  (-set-nodes [tg m] (assoc tg :nodes m))
   (-conj-node [tg k v] 
-    (digraph. (assoc nodes k v) 
-                (assoc sources k om/empty-ordered-map)  
-                (assoc sinks k om/empty-ordered-map)))
+    (-> tg
+        (assoc :nodes   (assoc nodes k v))
+        (assoc :sources (assoc sources k om/empty-ordered-map))
+        (assoc :sinks   (assoc sinks   k om/empty-ordered-map))))
   (-disj-node [tg k]
     (assert (contains? nodes k) (str "Node " k " does not exist!")) 
     (let [new-sources (reduce #(update-in %1 [%2] dissoc k)
                               (dissoc sources k)  (-get-sinks tg k))
           new-sinks   (reduce #(update-in %1 [%2] dissoc k)
                               (dissoc sinks k)  (-get-sources tg k))]
-      (digraph. (dissoc nodes k) new-sources new-sinks)))
+      (-> tg 
+          (assoc :nodes   (dissoc nodes k))
+          (assoc :sources new-sources)
+          (assoc :sinks   new-sinks))))
   (-has-node? [tg k]  (contains? nodes k))
   (-conj-arc  [tg source sink w]
     (let [w (or w 0)] ;ensure arcs have numeric weight, not nil
-      (digraph. nodes 
-                (update-in sources [sink]   assoc source w)
-                (update-in sinks   [source] assoc sink   w))))
+      (-> tg 
+          (assoc :sources (update-in sources [sink]   assoc source w))
+          (assoc :sinks   (update-in sinks   [source] assoc sink   w)))))
   (-disj-arc  [tg source sink]   
-    (digraph. nodes 
-      (assoc sources sink  (or (dissoc (get sources sink) source) 
-                                om/empty-ordered-map)) 
-      (assoc sinks   source (or (dissoc (get sinks source) sink) 
-                                om/empty-ordered-map))))
-  (-has-arc?  [tg source sink] (contains?   (get sources sink) source))
-  (-arc-weight [tg source sink] (when-let [snks (get sinks source)]
+    (-> tg 
+        (assoc :sources
+          (assoc sources sink  (or (dissoc (get sources sink) source) 
+                                   om/empty-ordered-map)))
+        (assoc :sinks
+          (assoc sinks   source (or (dissoc (get sinks source) sink) 
+                                    om/empty-ordered-map)))))
+  (-has-arc?    [tg source sink] (contains?   (get sources sink) source))
+  (-arc-weight  [tg source sink] (when-let [snks (get sinks source)]
                                   (get snks sink)))
-  (-get-arc   [tg source sink] [source sink (get-in sinks [source sink])])
-  (-get-sources [tg k] (vec (keys (get sources k))))
-  (-get-sinks [tg k]   (vec (keys (get sinks k)))))
+  (-get-arc     [tg source sink] [source sink (get-in sinks [source sink])])
+  (-get-sources [tg k]   (keys (get sources k)))
+  (-get-sinks   [tg k]   (keys (get sinks   k))))
+
+
+;;The old Digraph
+;;===============
+;; (defrecord digraph [nodes sources sinks]
+;;   ITopograph
+;;   (-get-nodes [tg] nodes)
+;;   (-set-nodes [tg m] (digraph. m sources sinks))
+;;   (-conj-node [tg k v] 
+;;     (digraph. (assoc nodes k v) 
+;;                 (assoc sources k om/empty-ordered-map)  
+;;                 (assoc sinks k om/empty-ordered-map)))
+;;   (-disj-node [tg k]
+;;     (assert (contains? nodes k) (str "Node " k " does not exist!")) 
+;;     (let [new-sources (reduce #(update-in %1 [%2] dissoc k)
+;;                               (dissoc sources k)  (-get-sinks tg k))
+;;           new-sinks   (reduce #(update-in %1 [%2] dissoc k)
+;;                               (dissoc sinks k)  (-get-sources tg k))]
+;;       (digraph. (dissoc nodes k) new-sources new-sinks)))
+;;   (-has-node? [tg k]  (contains? nodes k))
+;;   (-conj-arc  [tg source sink w]
+;;     (let [w (or w 0)] ;ensure arcs have numeric weight, not nil
+;;       (digraph. nodes 
+;;                 (update-in sources [sink]   assoc source w)
+;;                 (update-in sinks   [source] assoc sink   w))))
+;;   (-disj-arc  [tg source sink]   
+;;     (digraph. nodes 
+;;       (assoc sources sink  (or (dissoc (get sources sink) source) 
+;;                                 om/empty-ordered-map)) 
+;;       (assoc sinks   source (or (dissoc (get sinks source) sink) 
+;;                                 om/empty-ordered-map))))
+;;   (-has-arc?    [tg source sink] (contains?   (get sources sink) source))
+;;   (-arc-weight  [tg source sink] (when-let [snks (get sinks source)]
+;;                                   (get snks sink)))
+;;   (-get-arc     [tg source sink] [source sink (get-in sinks [source sink])])
+;;   (-get-sources [tg k]   (vec (keys (get sources k))))
+;;   (-get-sinks   [tg k]   (vec (keys (get sinks k)))))
 
 (def  empty-digraph     (->digraph {} {} {}))
 (defn invert-graph [g]  (->digraph (:nodes g) (:sinks g) (:sources g)))
+
+
+
+
+;;This is from flatland's ordered map implementation.  
+;; (delegating-deftype TransientOrderedMap 
+;;                     [^{:unsynchronized-mutable true, :tag ITransientMap} backing-map,  
+;;                      ^{:unsynchronized-mutable true, :tag ITransientVector} order] 
+;;                     {backing-map {ITransientMap [(count [])]}}  
+;;                     ITransientMap  
+;;                     (valAt [this k]    (.valAt this k nil))  
+;;                     (valAt [this k not-found]    
+;;                            (if-let [^MapEntry e (.valAt backing-map k)]      
+;;                              (.val e)      not-found))  
+;;                     (assoc [this k v]    
+;;                       (let [^MapEntry e (.valAt backing-map k this)         
+;;                             vector-entry (MapEntry. k v)          
+;;                             i (if (identical? e this)              
+;;                                 (do (change! order .conj vector-entry)                  
+;;                                     (dec (.count order)))              
+;;                                 (let [idx (.key e)]                
+;;                                   (change! order .assoc idx vector-entry)                
+;;                                   idx))]      
+;;                         (change! backing-map .conj (entry k v i)) this))
+;;                     (conj [this e]  
+;;                           (let [[k v] e] (.assoc this k v)))  
+;;                     (without [this k]   
+;;                           (let [^MapEntry e (.valAt backing-map k this)] 
+;;                             (when-not (identical? e this)      
+;;                               (let [i (.key e)]  
+;;                                 (change! backing-map dissoc! k)   
+;;                                 (change! order assoc! i nil)))    
+;;                             this))
+;;                     (persistent [this]    
+;;                                 (OrderedMap. (.persistent backing-map)
+;;                                              (.persistent order))))
+;; (defn transient-ordered-map [^OrderedMap om]  
+;;   (TransientOrderedMap. (.asTransient ^IEditableCollection (.backing-map om))     
+;;                         (.asTransient ^IEditableCollection (.order om))))
