b4b1b54a6663069e2642d2669cced10c18eef5df
Tom Spoon <spoon@blah.net>
2014-08-15 16:30:10 -0400

    Added make-string to general utils.

--- src/spork/util/general.clj
+++ src/spork/util/general.clj
@@ -358,6 +358,56 @@
 
 
 
+;;#Faster String Building
+;;clojure.core/string has a lot of inefficiencies, mostly due to 
+;;a lot of vararg usage.  If you're building strings a lot, i.e. 
+;;passing messages in a simulation using strings, or logging, etc.
+;;then make-string is roughly 5x faster.  It's a drop-in replacement 
+;;for clojure.core/str.
+(defn ^String simple-str [^Object x]
+  (if nil? x) "" (.toString x))
+
+(defmacro build-string [& args]
+  `(let [x#  ~(first args)
+         sb# (StringBuilder. (simple-str x#))]
+     (.toString
+      (doto sb#
+        ~@(for [a (rest args)]
+            `(.append (simple-str  ~a)))))))
+
+(def ^:constant +max-params+ 15)
+(defn string-func-body [n]
+  (let [obj (with-meta (gensym "obj") {:tag 'Object})]
+    (assert (>= n 0))
+    (if (<= n +max-params+)
+      (case n 
+        0 `(~(with-meta [] {:tag 'String}) "")
+        1 `(~(with-meta [obj] {:tag 'String})
+            (if (nil? ~obj) "" (. ~obj (toString))))    
+        (let [args (-> (vec (for [x (range n)] (gensym "str")))
+                       (with-meta {:tag String}))]    
+          `(~args
+            (build-string ~@args))))
+      (let [baseargs (vec (for [x (range (+ 2 +max-params+))] (gensym "str")))
+            args (-> baseargs
+                     (conj '&)
+                     (conj 'rest)
+                     (with-meta {:tag String}))] 
+      `(~args
+        (build-string (build-string ~@baseargs) 
+                      (apply clojure.core/str ~'rest)))))))
+
+;;Positional definitions of str, to eliminate arrayseq overhead due 
+;;to varargs version of str.  Since we're making lots of strings,
+;;it's stupid to incur the varargs cost here...
+(let [bodies (for [n (range (+ +max-params+ 2))]
+               (string-func-body n))]
+  (eval `(defn ~'make-string
+           "With no args, returns the empty string. With one arg x, returns
+            x.toString().  (str nil) returns the empty string. With more than
+            one arg, returns the concatenation of the str values of the args."
+           ~@bodies)))
+
 ;;More effecient memoization functions.  Clojure's built in memo 
 ;;memoizes args using a variadic code path, which forces the creation
 ;;of tons of arrayseqs....this is horrible for small, fast lookups.
