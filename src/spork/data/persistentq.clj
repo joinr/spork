a5dc613c98bab251e892d06785b1e3871adab5dd
Tom Spoon <spoon@blah.net>
2014-08-28 12:50:44 -0400

    Putting persistentq on hold.  For now, scheduling in sim.data has enough throughput for most needs.  However, we will need a higher throughput event scheduler for more fine-grained simulation.

--- src/spork/data/persistentq.clj
+++ src/spork/data/persistentq.clj
@@ -3,11 +3,13 @@
 ;;Without the benefit of being able to become a transient....
 ;;For my purposes, I'd like to have the queue transient-able...
 ;;So I'm re-implementing the persistentq, using a vector, 
-;;with a transient implementation.
+;;with a transient implementation.  
+;;*Work In Progress.
 (ns spork.data.persistentq)
 
 (defmacro unsupported-op []
   `(throw (Error. "unsupported operation")))
+(declare mutable-queue)
 
 (deftype queue [^clojure.lang.PersistentQueue q]  
   Object
@@ -45,9 +47,49 @@
   (size     [coll] (.count q))
   (isEmpty  [coll] (.isEmpty q))
   (contains [coll o] (.contains q o))
-  (iterator [coll] (.iterator q)))
+  (iterator [coll] (.iterator q))
+  clojure.lang.IEditableCollection
+  (asTransient [coll] (mutable-queue q)))
 
-(def ^:constant empty-queue (queue. clojure.lang.PersistentQueue/EMPTY))
+;; (deftype mqueue [^clojure.lang.ISeq f ^java.util.ArrayList r ^int cnt]  
+;;   Object
+;;   (hashCode [coll] (.hashCode q))
+;;   clojure.lang.Seqable
+;;   (seq [coll] (.seq q))
+;;   clojure.lang.ISeq
+;;   (first [coll] (.first q))
+;;   (next  [coll] (.next q))
+;;   (more  [coll] (.more q))
+;;   clojure.lang.IPersistentCollection 
+;;   (count [itm] (.count q))
+;;   (cons  [coll v]  (queue. (.cons q v )))
+;;   (empty [coll]    (queue. clojure.lang.PersistentQueue/EMPTY))
+;;   (equiv [this that] (.equiv q that))
+;;   clojure.lang.IPersistentStack 
+;;   (peek [coll] (.peek q))
+;;   (pop  [coll] (queue. (.pop q)))
+;;   clojure.lang.IPersistentList    
+;;   clojure.lang.IHashEq
+;;   (hasheq [coll] (.hasheq q))
+;;   clojure.lang.IObj
+;;   (meta [coll] (.meta q))
+;;   (withMeta [coll m] (queue. (.withMeta q m)))
+;;   java.util.Collection 
+;;   (^objects toArray [coll] (.toArray q))
+;;   (add     [coll o]   (unsupported-op))
+;;   (remove  [coll o]   (unsupported-op))
+;;   (addAll  [coll c]   (unsupported-op))
+;;   (clear   [coll]     (unsupported-op))
+;;   (removeAll [coll c] (unsupported-op))
+;;   (retainAll [coll c] (unsupported-op))
+;;   (containsAll [coll c] (every? #(contains? q %) c))
+;;   (^objects toArray  [coll ^objects a] (.toArray q a))
+;;   (size     [coll]   (.count q))
+;;   (isEmpty  [coll]   (.isEmpty q))
+;;   (contains [coll o] (.contains q o))
+;;   (iterator [coll]   (.iterator q)))
+
+;; (def ^:constant empty-queue (queue. clojure.lang.PersistentQueue/EMPTY))
   
   
   
