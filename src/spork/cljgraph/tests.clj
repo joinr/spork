4e098c3e7c498cddb3970aa0ef0e3948dc4855ea
Tom <Laptop@demo.com>
2015-01-06 09:03:32 -0500

    Inconsequential whitespace removal

--- src/spork/cljgraph/tests.clj
+++ src/spork/cljgraph/tests.clj
@@ -51,7 +51,6 @@
       (add-arcs (tree-arcs :q  
                            [:r :s :t :u :v :w :x :y :z :a1 :a2 :a3]))))
 
-
 (deftest graph-walks
   (is (= (ordered-nodes the-tree :a)
          [:a :b :e :h :k :l :f :i :m :n :o :c :d :g :j :p :q 
