f81f15a00ff8e029fba7d931d869925e03e4c6ea
tom <tom@that-place>
2015-08-31 10:37:50 -0500

    Got a wrapper around paintpanel implemented.  Works so far.

--- src/spork/cljgui/components/PaintPanel.clj
+++ src/spork/cljgui/components/PaintPanel.clj
@@ -1,64 +1,33 @@
 (ns spork.cljgui.components.PaintPanel
   (:gen-class
    :extends javax.swing.JPanel
-   :implements [clojure.lang.IMeta clojure.lang.IObj]
+   :implements [clojure.lang.IMeta clojure.lang.IObj clojure.lang.IDeref]
    :name    spork.cljgui.components.PaintPanel
+   :state   state
+   :init    init
+   :constructors {[] []}
    :exposes-methods {paintComponent parentPaintComponent
                      removeAll      parentRemoveAll
-                     removeNotify   parentRemoveNotify}))
+                     removeNotify   parentRemoveNotify})
+  (:import [java.awt Graphics]))
 
 (def painter (atom identity))
 (def metamap (atom {:paintf painter}))
 
 
+(defn -init [msg]  [[] (atom msg)])
+(defn -deref [this] @(.state this))
+
 (defn -paintComponent [this ^Graphics g]
   (do  (.parentPaintComponent this g)
        (@painter g)))
 
-(defn -removeNotify []
+(defn -removeNotify [this]
   (do (println "removing!")
-      (parentRemoveAll)
-      (parentRemoveNotify)))
+      (.parentRemoveAll this)
+      (.parentRemoveNotify this)))
 
-(defn -withMeta [m] (reset! meta-map m))
-(defn -meta [] @meta-map)       
+(defn -withMeta [m] (reset! metamap m))
+(defn -meta [this] @metamap)       
 
-;;Note -> if we don't call the parent's paintcomponent, swing will not 
-;;repaint the component like it should, which messes up scrollpanes
-;;and introduces graphical artifacts.
-(defn paintpanel
-  "Create a JPanel with its paint method overriden by paintf, which will be 
-   called using g.  We can get mutable behavior by passing a function that 
-   evals and applies a ref'd function for paintf, or we can keep the painting 
-   static.  Note, paintpanel is static, in that resize behavior will not scale
-   the original coordinate system.  It will NOT stretch.  Use paintpanel for 
-   simple situations where you have fixed dimensions."
-  ([width height paintf]
-   (let [painter  (atom paintf)
-         meta-map (atom {:paintf painter})
-         panel    (proxy [JPanel clojure.lang.IMeta clojure.lang.IObj] []
-                    (paintComponent [^Graphics g]
-                      (let [^JComponent this this
-                            _  (proxy-super paintComponent g)]
-                        (paintf g)))
-                    (removeNotify [] (do (println "removing!")
-                                         (proxy-super removeAll)
-                                         (proxy-super removeNotify)))
-                    (withMeta [m] (reset! meta-map m))
-                    (meta [] @meta-map))
-           savelistener (proxy [MouseAdapter] []
-                          (mouseClicked [^MouseEvent e]
-                             (if (= (.getButton e) MouseEvent/BUTTON3)
-                               (let [savepath 
-                                     (str (str (System/getProperty "user.home") 
-                                               "\\" "SavedBuffer.png")) ]
-                                 (do (let [buffer (jgraphics/make-imgbuffer width height)
-                                           bg     (j2d/bitmap-graphics buffer)
-                                           _      (paintf bg)]
-                                       (j2d/write-image buffer savepath nil))
-                                     (alert (str "Saved image to " savepath)))))))]                                      
-          (doto panel                                                     
-            (.setPreferredSize (Dimension. width height))
-            (.addMouseListener  savelistener)
-            (.setName  (str (gensym "Canvas"))))))
-  ([paintf] (paintpanel 250 250 paintf)))
+
--- src/spork/cljgui/components/swing.clj
+++ src/spork/cljgui/components/swing.clj
@@ -16,7 +16,8 @@
                      GridLayout Component Graphics Graphics2D Dimension Insets]           
            [java.awt.event ActionListener MouseListener ComponentListener 
             MouseAdapter MouseEvent WindowAdapter WindowEvent]
-           [java.awt.image BufferedImage]))
+           [java.awt.image BufferedImage]
+           [spork.cljgui.components.PaintPanel]))
 
  
 (defn get-events [obj]  (:event-stream (meta obj)))
@@ -783,6 +784,37 @@
                     (withMeta [m] (reset! meta-map m))
                     (meta [] @meta-map))
            savelistener (proxy [MouseAdapter] []
+                          (mouseClicked [^MouseEvent e]
+                             (if (= (.getButton e) MouseEvent/BUTTON3)
+                               (let [savepath 
+                                     (str (str (System/getProperty "user.home") 
+                                               "\\" "SavedBuffer.png")) ]
+                                 (do (let [buffer (jgraphics/make-imgbuffer width height)
+                                           bg     (j2d/bitmap-graphics buffer)
+                                           _      (paintf bg)]
+                                       (j2d/write-image buffer savepath nil))
+                                     (alert (str "Saved image to " savepath)))))))]                                      
+          (doto panel                                                     
+            (.setPreferredSize (Dimension. width height))
+            (.addMouseListener  savelistener)
+            (.setName  (str (gensym "Canvas"))))))
+  ([paintf] (paintpanel 250 250 paintf)))
+
+;;Note -> if we don't call the parent's paintcomponent, swing will not 
+;;repaint the component like it should, which messes up scrollpanes
+;;and introduces graphical artifacts.
+(defn new-paintpanel
+  "Create a JPanel with its paint method overriden by paintf, which will be 
+   called using g.  We can get mutable behavior by passing a function that 
+   evals and applies a ref'd function for paintf, or we can keep the painting 
+   static.  Note, paintpanel is static, in that resize behavior will not scale
+   the original coordinate system.  It will NOT stretch.  Use paintpanel for 
+   simple situations where you have fixed dimensions."
+  ([width height paintf]
+   (let [painter  (atom paintf)
+         meta-map (atom {:paintf painter})
+         panel    (PaintPanel.)
+         savelistener (proxy [MouseAdapter] []
                           (mouseClicked [^MouseEvent e]
                              (if (= (.getButton e) MouseEvent/BUTTON3)
                                (let [savepath 
