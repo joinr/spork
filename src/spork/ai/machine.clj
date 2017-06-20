0cdff384fbd8005dd0396e34514e77ea24389b13
joinr <vangelder@tantalus.net>
2017-06-20 17:07:53 -0500

    Transitiong fsm state over to ai/machine.

--- src/spork/ai/machine.clj
+++ src/spork/ai/machine.clj
@@ -2,9 +2,13 @@
 ;;Provides a set of data generally useful to any FSM.
 (ns spork.ai.machine)
 
+;'Provides a set of data useful to any FiniteStateMachine.
 (def inf (java.lang.Double/POSITIVE_INFINITY))
 
-(defrecord statedata [curstate ;Description of the current state
+;;we might want to get away from records due to readability issues.
+;;statedata could easily be map...although the records give use
+;;fast field access for free.
+(defrecord statedata [curstate
                       prevstate
                       nextstate 
                       timeinstate
@@ -15,71 +19,45 @@
                       statehistory])
 
 (def blank-data (statedata. nil nil nil 0 0 inf inf 0 [])) 
-  
+(def spawning-data (statedata. :spawning nil nil 0 0 inf inf 0 [])) 
 
-(defn change-state [fsm newstate & [newduration followingstate instant]]
+(defn change-state [^statedata fsm newstate & [newduration followingstate instant]]
   (merge fsm {:curstate newstate 
               :nextstate followingstate
               :statehistory (conj (:statehistory fsm) newstate)
               :duration newduration
               :timeinstate 0}))
 
+(defn ^statedata change-statedata [^statedata  sdata tostate duration followingstate]
+  (statedata. 
+   tostate
+   (.curstate sdata)
+   followingstate
+   0
+   (.timeinstate sdata)
+   duration
+   (.duration sdata)
+   (.statestart sdata)
+   (.cons ^clojure.lang.PersistentVector (.statehistory sdata) tostate)))
+
 (defn remaining [fsm]  
    (- (:duration fsm) (:timeinstate fsm)))
+    
+(defn progress [^statedata sd]
+  (let [duration    (.duration    sd)
+        timeinstate (.timeinstate sd)]
+    (if (and (pos? duration) (not= duration inf))
+      (double (/ timeinstate duration))
+      0)))
 
-            
+(defn add-duration [^statedata sd  amt]
+  (let [res  (+ (.timeinstate sd) amt)
+        diff (- (.duration sd) res)
+        ]
+    (if (not (neg? diff))
+      (assoc sd :timeinstate res)
+      (throw (Exception. (str ["time-in-state exceeds duration in fsm statedata."
+                               :tis      res
+                               :duration (.duration sd)
+                               :diff     diff]))))))
 
-;'Quick, instantaneous blips between state changes, usually with the intent to revert back soonafter
-;Public Sub BlipState(newstate As String, Optional followingstate As String)
-;
-;If CurrentState <> newstate Then
-;    StateHistory.add CurrentState
-;End If
-;
-;CurrentState = newstate
-;PreviousState = CurrentState
-;
-;If followingstate = vbNullString Then  'followingstate
-;    nextstate = followingstate
-;    duration = 0
-;End If
-;End Sub
-
-
-;'TODO -> wrap this into a list of states ....
-;'newstate
-;'Write now, it only goes back one, then cycles on infinitely between 2 states, only really good for
-;'quick blips, but it's efficient
-
-;Public Sub RevertState()
-;Dim tmpstate As String
-;Dim tmpdur As Single
-;Dim tmptimeinstate As Single
-;
-;StateHistory.add CurrentState
-;
-;tmpdur = duration
-;tmpstate = CurrentState
-;tmptimeinstate = timeinstate
-;
-;
-;CurrentState = PreviousState
-;duration = DurationPrior
-;timeinstate = timeinstatePrior
-;
-
-;'PreviousState
-;'DurationPrior
-;'tmpstate
-;'tmpdur
-;StateHistory.add CurrentState 'shows the reversion
-;End Sub
-
-(defn progress [{:keys [duration timeinstate] :as sd}] 
-  (if (and (pos? duration) (not= duration inf))
-    (double (/ timeinstate duration))
-    0))
-
-
-(defn add-duration [{:keys [timeinstate] :as sd} amt]
-  (assoc sd :timeinstate (+ timeinstate amt)))
